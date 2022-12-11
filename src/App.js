ce92c3e605792bec4a57115341395b002e7b20a4
Anirban Dutta <63803805+anirban-1994@users.noreply.github.com>
2022-12-11 18:44:24 +0530

    image data url load successfully

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Carousel } from "./Components/Carousel";
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col w-screen h-screen items-center justify-around">
+      <p className="text-2xl font-mono font-bold">Welcome to demo page</p>
       <Carousel/>
     </div>
   );
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,3 +1,22 @@
-export const Carousel = ()=>{
-    return <p>Message from Carousel</p>
-}
+import { useEffect, useState } from "react";
+
+export const Carousel = () => {
+  const [imageURL, setImageURL] = useState([]);
+  const [currentImageNo, setCurrentImageNo] = useState(0);
+
+  //load image url after mount
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+      .then((response) => response.json())
+      .then((json_data) => {
+        let image_url = json_data.map((product) => product.url);
+        setImageURL(image_url);
+      });
+  }, []);
+
+  return (
+    <div className="w-[80%] min-w-[100px] max-w-[500px] aspect-square border border-gray-500 rounded-2xl p-2">
+      <img src={imageURL[currentImageNo]} alt="" className="rounded-2xl" />
+    </div>
+  );
+};
