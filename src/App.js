c5bbd96ed4334987f8eacd513527394d44945095
Anirban Dutta <63803805+anirban-1994@users.noreply.github.com>
2022-12-11 19:04:13 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Carousel } from "./Components/Carousel";
 
-
 function App() {
   return (
     <div className="flex flex-col w-screen h-screen items-center justify-around">
-      <p className="text-2xl font-mono font-bold">Welcome to demo page</p>
-      <Carousel/>
+      <Carousel />
     </div>
   );
 }
